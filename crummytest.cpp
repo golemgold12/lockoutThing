dd2e4fb8853295159d35f765cf5d16eb95f92244
Larsen <larfreund25@gmail.com>
2025-05-30 11:56:37 -0700

    revision attempt 1

--- crummytest.cpp
+++ crummytest.cpp
@@ -2,17 +2,28 @@
 #include <curl/curl.h>
 #include <string>
 
-//g++ crummytest.cpp -o crummytest -I./vcpkg/installed/x64-mingw-static/include -L./vcpkg/installed/x64-mingw-static/lib -lcurl -lws2_32 -lbcrypt -lz -lcrypt32 -lws2_32 -lwinmm -luser32 -lgdi32 -ladvapi32
-// Please write your own test program! Don't use this.
+// g++ crummytest.cpp -o crummytest -I./vcpkg/installed/x64-mingw-static/include -L./vcpkg/installed/x64-mingw-static/lib -lcurl -lws2_32 -lbcrypt -lz -lcrypt32 -lws2_32 -lwinmm -luser32 -lgdi32 -ladvapi32
 
-void startItUp(){
+void startItUp()
+{
     std::cout << "Starting test program.";
     std::cout << "\nSee canvas for help with 1st user and pass.";
 }
-void epicFail(){
+void epicFail()
+{
     std::cout << "We got no response- ensure the server is set up....";
 }
-int main() {
+
+size_t writeCallback(void *contents, size_t size, size_t newMember, std::string *out)
+{
+    size_t totalSize = size * newMember;
+    out->append((char *)contents, totalSize);
+    std::cout << "Callback written to us.";
+    return totalSize;
+}
+
+int main()
+{
 
     startItUp();
     CURL *curl;
@@ -21,8 +32,9 @@ int main() {
     curl_global_init(CURL_GLOBAL_ALL);
     curl = curl_easy_init();
 
-    if (curl) {
-        const std::string json_data = R"(["superAdmin1999", "testAdmin30!", "Nolan80", "MackTruck12"])"; // your first and secound string should be a admin username and password
+    if (curl)
+    {
+        const std::string json_data = R"(["Puppies","superAdmin1999", "testAdmin30!", "Nolan80", "MackTruck12"])"; // your first and secound string should be a admin username and password
 
         curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:5023/LogProcess");
         curl_easy_setopt(curl, CURLOPT_POSTFIELDS, json_data.c_str());
@@ -33,30 +45,32 @@ int main() {
 
         // For response
         std::string response_string;
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, [](void *contents, size_t size, size_t nmemb, std::string *out) {
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, [](void *contents, size_t size, size_t nmemb, std::string *out)
+                         {
             size_t totalSize = size * nmemb;
             out->append((char *)contents, totalSize);
-            return totalSize;
-        });
+            return totalSize; });
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeCallback);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response_string);
-
         res = curl_easy_perform(curl);
-        if (res != CURLE_OK) {
-            std::cerr << "our curl_easy_perform() failed: " << curl_easy_strerror(res) << "\n";
-
-        } else {
-            //std::cout << "We got a response from the server" << response_string << "\n";
+        if (res != CURLE_OK)
+        {
+            std::cerr << "\nOur curl_easy_perform() failed: " << curl_easy_strerror(res) << "\n";
+        }
+        else
+        {
+            std::cout << "\n We got: " << response_string << std::endl;
             itWorked = true;
         }
 
         curl_slist_free_all(headers);
         curl_easy_cleanup(curl);
     }
-    if(itWorked == false){
+    if (itWorked == false)
+    {
 
         epicFail();
     }
     curl_global_cleanup();
     return 0;
 }
-
--- main.cpp
+++ main.cpp
@@ -6,7 +6,17 @@
 #include <fstream>
 #include <crow.h>
 #include <string.h>
-//  COMPILE VIA: g++ main.cpp -IC:\curl\include -LC:\curl\lib -lcurl -o SecurityCheck.exe
+
+/* 
+Structure requests to this program like this: 
+["username", "password", "Username of Acconut", "Username of account"...]
+Structure your server responses like this:
+200 - success
+400 - Account not found
+401 - No admin credentials
+403 - Bad Password
+423 - Account is locked
+*/
 size_t writeCallback(void *contents, size_t size, size_t newMember, std::string *out)
 {
     size_t totalSize = size * newMember;
@@ -15,15 +25,19 @@ size_t writeCallback(void *contents, size_t size, size_t newMember, std::string 
     return totalSize;
 }
 
-void ReadAndTakeAction(const std::vector<std::string> &usernames)
-{
+int ReadAndTakeAction(const std::vector<std::string> &usernames, const std::string passCheck)
+{    
 
+    if(usernames[0] != passCheck){
+
+        return 2;
+    }
     // note that the first name will be the password for the admin
     curl_global_init(CURL_GLOBAL_ALL); // initialize!
     std::ostringstream newString;
 
     newString << "[";
-    for (size_t i = 0; i < usernames.size(); i++)
+    for (size_t i = 1; i < usernames.size(); i++)
     {
 
         newString << "\"" << usernames[i] << "\"";
@@ -36,7 +50,7 @@ void ReadAndTakeAction(const std::vector<std::string> &usernames)
     }
     newString << "]";
     std::string jsonArg = newString.str();
-    std::cout << jsonArg;
+    //std::cout << jsonArg;
     CURL *curl = curl_easy_init();
 
     if (curl && newString)
@@ -44,86 +58,151 @@ void ReadAndTakeAction(const std::vector<std::string> &usernames)
         CURLcode res;
         struct curl_slist *headers = NULL;
 
-        curl_easy_setopt(curl, CURLOPT_URL, "http://classwork.engr.oregonstate.edu:5012/api/lockup"); // No idea atm for security
-
+        /*
+        You don't have to just use my site. This can work anywhere as long as you follow the error codes.
+        */
+        curl_easy_setopt(curl, CURLOPT_URL, "http://classwork.engr.oregonstate.edu:5012/api/lockup");  
+        
+        
         headers = curl_slist_append(headers, "Content-Type: application/json");
         curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
 
         curl_easy_setopt(curl, CURLOPT_POSTFIELDS, jsonArg.c_str());
-        // now we do response
+       
 
         std::string responseString;
+        // now we do response
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeCallback);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &responseString);
 
         res = curl_easy_perform(curl);
-        if (res != CURLE_OK)
-            std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << " \n";
+        if (res != CURLE_OK){
+            std::cerr << "Curl failed to connect to server with error: " << curl_easy_strerror(res) << " \n";
+            return 0;
+        }
+          
         else
         {
-            if (responseString != "")
-            {
-                std::cout << "Successfully wrote message to the server";
-             
-            }
+            long HTTPCodeResponse = 0;
+            curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &HTTPCodeResponse);
+            curl_slist_free_all(headers);
+            curl_easy_cleanup(curl);
+            return HTTPCodeResponse;
+
+
         }
 
-        curl_slist_free_all(headers);
-        curl_easy_cleanup(curl);
+      
     }
-    return;
+    return 0;
+
 }
 
-int programRun()
+int programRun(std::string passCheck)
 {
- 
 
     crow::SimpleApp crowSimple;
 
-    CROW_ROUTE(crowSimple,"/LogProcess").methods(crow::HTTPMethod::Post)([](const crow::request & crowreq){
+    CROW_ROUTE(crowSimple,"/LogProcess").methods(crow::HTTPMethod::Post)([passCheck](const crow::request & crowreq){
 
         crow::json::rvalue requestBody = crow::json::load(crowreq.body);
-        if(requestBody){
-            std::cout << "We've recieved some form of message ";
-          
+
+        if(!requestBody){
+            crow::json::wvalue badJSON;
+            badJSON["status"] = "error";
+            badJSON["message"] = "Your request had data that isn't JSON. Please format it accordingly.";
+            return crow::response(400, badJSON);
+        }
+        else{
+            
             std::vector<std::string> ourusernames;
+
             for(const auto&  val : requestBody){
+
+            
+           
                 ourusernames.push_back(val.s());
+                
 
             }
-            //std::vector<std::string> ourUsers = {"superAdmin1999", "testAdmin30", "Nolan80", "MackTruck12"}; // test
+          
 
-            //ReadAndTakeAction(ourUsers);
-            ReadAndTakeAction(ourusernames);
+            try{
+                int loginAttempt = ReadAndTakeAction(ourusernames,passCheck);
+                crow::json::wvalue acceptedJSON;
 
-            return crow::response(200, "\nFound and processes usernames also WHY are we using crow");
-        } else{
+                switch(loginAttempt){
+                    case 0:
+                        acceptedJSON["status"] = "error";
+                        acceptedJSON["message"] = "Failure to connect.";
+                        return crow::response(200, acceptedJSON);
 
-            return crow::response(400, "\nBad Request!");
-        }
+                    break;
+                    case 2:
+                        acceptedJSON["status"] = "error";
+                        acceptedJSON["message"] = "User passkey for this progam wasn't correct";
+                        return crow::response(401, acceptedJSON);
+
+                    break;
+                    case 200:
+                        acceptedJSON["status"] = "success";
+                        acceptedJSON["message"] = "Usernames processed successfully.";
+                        return crow::response(200, acceptedJSON);
+
+                    break;
+                    case 400:
+                        acceptedJSON["status"] = "error";
+                        acceptedJSON["message"] = "User doesen't exist";
+                        return crow::response(400, acceptedJSON);
+
+                    break;
+
+                    case 401:
+                        acceptedJSON["status"] = "error";
+                        acceptedJSON["message"] = "User isn't a admin"; 
+                        return crow::response(401, acceptedJSON);
+
+                    break;
+                    case 403:
+                        acceptedJSON["status"] = "error";
+                        acceptedJSON["message"] = "Acceptable admin user, but bad password."; 
+                        return crow::response(401, acceptedJSON);
+
+                    break;
+
+                    case 423:
+                        acceptedJSON["status"] = "error";
+                        acceptedJSON["message"] = "User account is locked"; 
+                        return crow::response(423, acceptedJSON);
+
+                    break;
+
+                    default:
+                        acceptedJSON["status"] = "error";
+                        acceptedJSON["message"] = "No criteria met, refer to code to structure your errors";
+                        return crow::response(500, acceptedJSON);
+                    break;
+                }
+            } catch(const std::exception& ex){
+
+                crow::json::wvalue badJSON;
+                badJSON["status"] = "error";
+                badJSON["message"] = "Rare Error- something is wrong with the main code";
+                 return crow::response(500, badJSON);
+            }
+            
+        } 
 
     });
     crowSimple.port(5023).multithreaded().run();
 
-    /*
-    Someting here
-
-    */
-    std::cout << "\nEnded program.";
+  
+    std::cout << "\nCtrl-C pressed, ending program.";
     return 0;
 }
 int main()
 {
-
-    std::cout << "\n\n Starting Program \n\n";
-    int testShot = programRun();
-    if (testShot)
-    {
-
-        return 0;
-    }
-    else
-    {
-        return 1;
-    }
+    std::string superSecret = "Puppies";
+    std::cout << "\n\n ----Starting Program---- \n\n";
+    int testShot = programRun(superSecret);
 }
